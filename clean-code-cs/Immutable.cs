3e3a004f294783f1d04c58620d71dd5e6aeb2821
Victor Rentea <victorrentea@gmail.com>
2025-03-04 15:08:26 +0200

    Immutable redesign

--- clean-code-cs/Immutable.cs
+++ clean-code-cs/Immutable.cs
@@ -10,29 +10,53 @@ namespace Victor.Training.Cleancode
         {
             ImmutableList<int> list = new[] { 1, 2, 3 }.ToImmutableList();
 
-            Immutable immutable = new(1, 2, list, new Other(15));
+            Immutable immutable = new(new(1, 2), list, new Other(15));
             Console.WriteLine("Before: " + immutable);
 
-            Wilderness(immutable);
-            list.Clear();
+            Point newPosition = Wilderness(immutable); // avoid reassigning variable for other meanings.
+            Immutable translated = immutable with { Point = newPosition };
 
-            Console.WriteLine("After:  " + immutable);
+           
+            Console.WriteLine("After:  " + translated);
         }
 
-        private static void Wilderness(Immutable immutable)
+        private static Point Wilderness(Immutable immutable)
         {
-            // dark, deep logic not expected to change the immutable object x,y
+            // dark, deep logic not expected to change the immutable object x+1,y+1
             //immutable.GetList().Clear();
+            //immutable.X++;
+
+            //Immutable copy = immutable with
+            //{
+            //    X = immutable.X + 1,
+            //    Y = immutable.Y + 1
+            //};
+
+            return immutable.Point.Translate(+1, +1);
         }
     }
+    public readonly record struct Point(int X, int Y)
+    {
+        internal Point Translate(int dx, int dy)
+        {
+            return this with
+            {
+                X = X + dx,
+                Y = Y + dy
+            };
+        }
+    }  
 
-    // shallow immutable
+    // deep immutable
     public readonly record struct Immutable(
-        int X,
-        int Y,
+        //int X,
+        //int Y,
+        Point Point,
         ImmutableList<int> List,
         Other Other)
-    { }
+    {
+        
+    }
 
 
     public readonly record struct Other(int a) { }
