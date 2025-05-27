91c9ba615fbd4c30913baf682ba6d2baaa64bd18
Slavomir Barlov <s.barlov@sks365.com>
2025-05-27 14:55:42 +0200

    refactoring part 3

---
+++ clean-code-cs/Rental.cs
@@ -0,0 +1,33 @@
+﻿namespace Victor.Training.Cleancode.VideoStore;
+
+public record Rental(Movie Movie, int NumberOfDays)
+{
+    public bool IsEligibleForBonus => Movie.MovieType == MovieType.NewRelease && NumberOfDays > 1;
+    public decimal Amount()
+    {
+        var amount = 0m;
+        switch (Movie.MovieType)
+        {
+            case MovieType.Regular:
+                amount += 2;
+                if (NumberOfDays > 2)
+                {
+                    amount += (NumberOfDays - 2) * 1.5m;
+                }
+                break;
+            case MovieType.NewRelease:
+                amount += NumberOfDays * 3;
+                break;
+            case MovieType.Children:
+                amount += 1.5m;
+                if (NumberOfDays > 3)
+                {
+                    amount += (NumberOfDays - 3) * 1.5m;
+                }
+                break;
+        }
+
+        return amount;
+    }
+
+}
--- clean-code-cs/VideoStore.cs
+++ clean-code-cs/VideoStore.cs
@@ -3,60 +3,22 @@ using static Victor.Training.Cleancode.VideoStore.Customer;
 
 namespace Victor.Training.Cleancode.VideoStore
 {
-    public class Movie
-    {
-        public MovieType MovieType { get; set; }
-        public string Title { get; }
 
-        public Movie(string title, MovieType movieType)
-        {
-            Title = title;
-            MovieType = movieType;
-        }
-    }
+    public record Movie(string Title, MovieType MovieType);
 
     public class Customer
     {
         private readonly List<Rental> _rentals = new();
         public string Name { get; }
-        public int FrequentRenterPoints => _rentals.Count + _rentals.Where(x => x.IsEligibleForBonus).Count();
+        public int FrequentRenterPoints => _rentals.Count + _rentals.Count(x => x.IsEligibleForBonus);
+        public decimal TotalAmount => _rentals.Sum(rental => rental.Amount());
 
         public Customer(string name)
         {
             Name = name;
         }
 
-        public record Rental(Movie Movie, int NumberOfDays)
-        {
-            public bool IsEligibleForBonus => Movie.MovieType == MovieType.NewRelease && NumberOfDays > 1;
-            public decimal Amount()
-            {
-                var amount = 0m;
-                switch (Movie.MovieType)
-                {
-                    case MovieType.Regular:
-                        amount += 2;
-                        if (NumberOfDays > 2)
-                        {
-                            amount += (NumberOfDays - 2) * 1.5m;
-                        }
-                        break;
-                    case MovieType.NewRelease:
-                        amount += NumberOfDays * 3;
-                        break;
-                    case MovieType.Children:
-                        amount += 1.5m;
-                        if (NumberOfDays > 3)
-                        {
-                            amount += (NumberOfDays - 3) * 1.5m;
-                        }
-                        break;
-                }
-
-                return amount;
-            }
-
-        }
+        
 
         public void AddRental(Movie movie, int numberOfDays)
         {
@@ -65,31 +27,27 @@ namespace Victor.Training.Cleancode.VideoStore
 
         public string Statement()
         {
-            var totalAmount = CalculateTotalAmount();
 
             var result = "Rental Record for " + Name + "\n";
-            foreach (var rental in _rentals) { 
-                result += "\t" + rental.Movie.Title + "\t" + rental.Amount().ToString("0.0", CultureInfo.InvariantCulture) + "\n";
-            }
+            result += GetBody();
 
             // add footer lines
-            result += "Amount owed is " + totalAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\n";
-            result += "You earned " + FrequentRenterPoints.ToString() + " frequent renter points\n";
+            result += "Amount owed is " + TotalAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\n";
+            result += "You earned " + FrequentRenterPoints + " frequent renter points\n";
 
             return result;
         }
 
-        private decimal CalculateTotalAmount()
+        private string GetBody()
         {
-            var totalAmount = 0m;
+            var result = string.Empty;
             foreach (var rental in _rentals)
             {
-                //dtermines the amount for each line
-                var rentalAmount = rental.Amount();
-
-                totalAmount += rentalAmount;
+                result += "\t" + rental.Movie.Title + "\t" + rental.Amount().ToString("0.0", CultureInfo.InvariantCulture) +
+                          "\n";
             }
-            return totalAmount;
+
+            return result;
         }
     }
 }
