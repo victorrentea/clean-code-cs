9e38873b9d4d622a1b674839b03560ba454b0171
Saverio Trombini <saverio.trombini@cgm.com>
2025-05-27 15:56:16 +0200

    refactoring part 5

---
+++ clean-code-cs-test/RentalTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Victor.Training.Cleancode.VideoStore;
+
+namespace clean_code_cs_test
+{
+    internal class RentalTests
+    {
+
+        [TestCase("Star Wars", MovieType.NewRelease, 6, 18.0)]
+ //       [TestCase("Star Wars", MovieType.NewRelease, 6, 18.0)]
+        public void AmountCalculationTest(string movieName, MovieType movieType, int days, decimal expectedAmount) 
+        { 
+            var movie = new Movie(movieName, movieType);    
+            var rental = new Rental(movie, days);
+            var amount = rental.Amount;
+            Assert.AreEqual(expectedAmount, amount);
+        }
+    }
+}
