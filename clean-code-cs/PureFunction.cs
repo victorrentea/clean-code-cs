fa54663d6b2f6f39893cb12b26f0ff3bac213120
Victor Rentea <victorrentea@gmail.com>
2025-05-27 11:45:19 +0300

    this code violates Separation by "Layers of Abstraction" = a method/class is either high-level policy(birds-eye view workflow) or low-level detail(complex parts of that larger flow)

--- clean-code-cs/PureFunction.cs
+++ clean-code-cs/PureFunction.cs
@@ -19,23 +19,40 @@ namespace Victor.Training.Cleancode
             Customer customer = customerRepo.FindById(customerId); // SELECT will throw if null Customer comes back if you enable null check per project
             List<Product> products = productRepo.FindAllById(productIds); // SELECT .. WHERE ID IN (?,?..?)
 
+            //Dictionary<long, double> initialPrices = ResolveInitialPrices(thirdPartyPricesApi, internalPrices, products);
 
+            Dictionary<long, double> initialPrices = new();
+            foreach (Product product in products)
+            {
+                double? price = internalPrices.ContainsKey(product.Id) ? internalPrices[product.Id] : null;
+                if (price == null)
+                {
+                    price = thirdPartyPricesApi.FetchPrice(product.Id); // REST API call
+                }
+                initialPrices[product.Id] = (double)price; // TODO ask biz: what if not found in either internalPrices nor thirdPartyApi
+            }
 
-            Dictionary<long, double> initialPrices = ResolveInitialPrices(thirdPartyPricesApi, internalPrices, products);
-
-            // apply coupons to determine the final prices
-            var result = ApplyCouponse(customer, products, initialPrices);
+            
+            var result = ApplyCoupons(customer.Coupons, products, initialPrices);
 
             couponRepo.MarkUsedCoupons(customerId, result.UsedCoupons); // INSERT
             return result.FinalPrices;
         }
-        // TODO perhaps a record
-        record class PriceCalculationResult(
-            Dictionary<long, double> FinalPrices,
-            List<Coupon> UsedCoupons);
 
-        private static PriceCalculationResult ApplyCouponse(
-            Customer customer,
+        // TODO perhaps a record
+        //record class PriceCalculationResult(
+        //    Dictionary<long, double> FinalPrices,
+        //    List<Coupon> UsedCoupons);
+
+        // this is a PURE function: same result
+
+        // Victor is fine unit testing this COMPLEX PURE function independently w/o mocking
+        // Data IN -> data out = best type of test strategy for complex logic
+        // holy grail of unit testing => decouple high complexity
+        // a) traditional: move to another class as a public method
+        // b) hacky: open this method just for tests = "subcutaneous tests"
+        internal static (Dictionary<long, double> FinalPrices, List<Coupon> UsedCoupons) ApplyCoupons(
+            List<Coupon> coupons,
             List<Product> products,
             Dictionary<long, double> initialPrices)
         {
@@ -44,17 +61,17 @@ namespace Victor.Training.Cleancode
             foreach (Product product in products)
             {
                 var price = initialPrices[product.Id];
-                foreach (Coupon coupon in customer.Coupons)
+                foreach (Coupon coupon in coupons)
                 {
                     if (coupon.AutoApply && coupon.IsApplicableFor(product) && !usedCoupons.Contains(coupon))
                     {
                         price = coupon.Apply(product, price);
-                        usedCoupons.Add(coupon);// TODO can we first select coupons to apply in PASS1, and in PASS2 apply them?
+                        usedCoupons.Add(coupon); // TODO can we first select coupons to apply in PASS1, and in PASS2 apply them?
                     }
                 }
                 finalPrices[product.Id] = price;
             }
-            return new PriceCalculationResult(finalPrices, usedCoupons);
+            return (finalPrices, usedCoupons);
         }
 
         private static Dictionary<long, double> ResolveInitialPrices(IThirdPartyPricesApi thirdPartyPricesApi, Dictionary<long, double> internalPrices, List<Product> products)
