49d1d90bf2c167a82339ef74ce295ca6bfd288e9
Momir123 <momo.zecevic@gmail.com>
2025-05-27 14:50:07 +0200

    mz

--- clean-code-cs/VideoStore.cs
+++ clean-code-cs/VideoStore.cs
@@ -13,17 +13,9 @@ namespace Victor.Training.Cleancode.VideoStore
 
     public class Customer
     {
-        private const decimal RegularPriceBase = 2;
-        private const decimal RegularPriceExtraDailyRate = 1.5m;
-        private const int RegularPriceExtraDaysThreshold = 2;
+        
 
-        private const int NewReleaseDailyRate = 3;
-
-        private const decimal ChildrensPriceBase = 1.5m;
-        private const int ChildrensPriceExtraDaysThreshold = 3;
-        private const decimal ChildrensPriceExtraDailyRate = 1.5m;
-
-        private readonly List<(Movie Movie, decimal DaysRented)> _rentals = new();
+        private readonly List<(Movie Movie, int DaysRented)> _rentals = new();
         public string Name { get; }
 
         public Customer(string name)
@@ -36,7 +28,7 @@ namespace Victor.Training.Cleancode.VideoStore
             _rentals.Add((movie, daysRented));
         }
 
-        public string Statement()
+        public string PrintStatement()
         {
             var frequentRenterPoints = 0;
             var totalAmount = 0m;
@@ -44,50 +36,72 @@ namespace Victor.Training.Cleancode.VideoStore
 
             foreach (var rental in _rentals)
             {
-                var priceAmount = 0m;
+                var rentalAmount = DetermineRentalAmount(rental);
+                frequentRenterPoints = AddFrequentRenterPoints(frequentRenterPoints, rental);
 
-                //dtermines the amount for each line
-                switch (rental.Movie.PriceCode)
-                {
-                    case PriceCode.Regular:
-                        priceAmount += RegularPriceBase;
-                        if (rental.DaysRented > RegularPriceExtraDaysThreshold)
-                        {
-                            priceAmount += (rental.DaysRented - RegularPriceExtraDaysThreshold) * RegularPriceExtraDailyRate;
-                        }
-                        break;
-                    case PriceCode.NewRelease:
-                        priceAmount += rental.DaysRented * NewReleaseDailyRate;
-                        break;
-                    case PriceCode.Childrens:
-                        priceAmount += ChildrensPriceBase;
-                        if (rental.DaysRented > ChildrensPriceExtraDaysThreshold)
-                        {
-                            priceAmount += (rental.DaysRented - ChildrensPriceExtraDaysThreshold) * ChildrensPriceExtraDailyRate;
-                        }
-                        break;
-                }
-
-                frequentRenterPoints++;
-
-                // add bonus for a two day new release rental
-                if (rental.Movie.PriceCode == PriceCode.NewRelease
-                    && rental.DaysRented > 1)
-                {
-                    frequentRenterPoints++;
-                }
-
-                result += "\t" + rental.Movie.Title + "\t" + priceAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\n";
-                totalAmount += priceAmount;
+                result += $"\t{rental.Movie.Title}\t{rentalAmount.ToString("0.0", CultureInfo.InvariantCulture)}\n";
+                totalAmount += rentalAmount;
             }
 
             // add footer lines
-            result += "Amount owed is " + totalAmount.ToString("0.0", CultureInfo.InvariantCulture) + "\n";
-            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points\n";
+            result += $"Amount owed is {totalAmount.ToString("0.0", CultureInfo.InvariantCulture)}\n";
+            result += $"You earned {frequentRenterPoints} frequent renter points\n";
 
             return result;
         }
 
+        private static int AddFrequentRenterPoints(int frequentRenterPoints, (Movie Movie, int DaysRented) rental)
+        {
+            frequentRenterPoints++;
+
+            if (rental.Movie.PriceCode == PriceCode.NewRelease
+                && rental.DaysRented > 1)
+            {
+                frequentRenterPoints++;
+            }
+
+            return frequentRenterPoints;
+        }
+
+        private static decimal DetermineRentalAmount((Movie Movie, int DaysRented) rental)
+        {
+            const decimal RegularPriceBase = 2;
+            const decimal RegularPriceExtraDailyRate = 1.5m;
+            const int RegularPriceExtraDaysThreshold = 2;
+
+            const int NewReleaseDailyRate = 3;
+
+            const decimal ChildrensPriceBase = 1.5m;
+            const int ChildrensPriceExtraDaysThreshold = 3;
+            const decimal ChildrensPriceExtraDailyRate = 1.5m;
+
+            decimal rentalAmount = 0m;
+
+            switch (rental.Movie.PriceCode)
+            {
+                case PriceCode.Regular:
+                    rentalAmount += RegularPriceBase;
+                    if (rental.DaysRented > RegularPriceExtraDaysThreshold)
+                    {
+                        rentalAmount += (rental.DaysRented - RegularPriceExtraDaysThreshold) * RegularPriceExtraDailyRate;
+                    }
+                    break;
+                case PriceCode.NewRelease:
+                    rentalAmount += rental.DaysRented * NewReleaseDailyRate;
+                    break;
+                case PriceCode.Childrens:
+                    rentalAmount += ChildrensPriceBase;
+                    if (rental.DaysRented > ChildrensPriceExtraDaysThreshold)
+                    {
+                        rentalAmount += (rental.DaysRented - ChildrensPriceExtraDaysThreshold) * ChildrensPriceExtraDailyRate;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(rental.Movie.PriceCode));
+            }
+
+            return rentalAmount;
+        }
     }
 }
 
